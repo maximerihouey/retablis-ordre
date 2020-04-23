e7b4ed236c5ee1f8bbf1b10028f25da80d4dc72a
Maxime Rihouey <maxime.rihouey@tinyclues.com>
2020-04-23 22:45:26 +0200

    Updates

---
+++ precache-manifest.6fdfcca3756f3e8af07e6908f713d553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7ad341ab1e3b6580e3340e347d6488",
+    "url": "/retablis-ordre/index.html"
+  },
+  {
+    "revision": "a47cca449e42644f3f3b",
+    "url": "/retablis-ordre/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e484770f8c2f44e29087",
+    "url": "/retablis-ordre/static/css/main.9a4c6c54.chunk.css"
+  },
+  {
+    "revision": "a47cca449e42644f3f3b",
+    "url": "/retablis-ordre/static/js/2.49a1ba58.chunk.js"
+  },
+  {
+    "revision": "3ebd1f1736a50eb6d86eb374a63c9611",
+    "url": "/retablis-ordre/static/js/2.49a1ba58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e484770f8c2f44e29087",
+    "url": "/retablis-ordre/static/js/main.004bc4da.chunk.js"
+  },
+  {
+    "revision": "3c3769ce4fa4d4d57f3a",
+    "url": "/retablis-ordre/static/js/runtime-main.b16f098a.js"
+  }
+]);
